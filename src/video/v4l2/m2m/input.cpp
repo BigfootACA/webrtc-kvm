db4332e1f1f3b5c10f2c2d807307d33f9cbb15ff
BigfootACA <bigfoot@classfun.cn>
2024-08-17 20:09:53 +0800

    video: v4l2: m2m: fix memory to memory queue and dequeue
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- src/video/v4l2/m2m/input.cpp
+++ src/video/v4l2/m2m/input.cpp
@@ -12,13 +12,18 @@ void V4L2MemoryToMemory::OnProcessInput(StreamBuffer*input){
 	static bool warned=false;
 	if(unlikely(out->buffers.size()!=cap->buffers.size()))
 		throw RuntimeError("output buffers and capture buffers mismatch");
-	if(auto buffer=out->FindAvailableBuffer()){
-		out->QueueInput(input,buffer);
-		cap->QueueBuffer(cap->buffers[buffer->idx]);
-		if(unlikely(poll_id.IsNull()))EnablePoll();
-		warned=false;
-	}else{
-		if(!warned)log_warn("stream {} no available buffer",GetID());
-		warned=true;
+	auto buffer=out->FindAvailableBuffer();
+	if(!buffer){
+		out->DequeueBuffer(&buffer);
+		if(!buffer){
+			if(!warned)log_warn("stream {} no available buffer",GetID());
+			warned=true;
+			return;
+		}
+		if(buffer->status==STATUS_DEQUEUED)buffer->status=STATUS_IDLE;
 	}
+	out->QueueInput(input,buffer);
+	cap->QueueBuffer(cap->buffers[buffer->idx]);
+	if(unlikely(poll_id.IsNull()))EnablePoll();
+	warned=false;
 }
