e70b78ce9c550985f1bc843de902ccea8fd4f426
BigfootACA <bigfoot@classfun.cn>
2024-08-17 19:43:30 +0800

    http: httpd.cpp: add external logger for microhttpd
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- src/http/httpd.cpp
+++ src/http/httpd.cpp
@@ -17,16 +17,30 @@ static void handle_complete(
 	if(*req_cls)free(*req_cls);
 }
 
+static void handle_log(
+	void*cls,
+	const char*fm,
+	va_list ap
+){
+	char*buf=nullptr;
+	vasprintf(&buf,fm,ap);
+	if(!buf)return;
+	log::print(log::LOG_INFO,buf);
+	free(buf);
+}
+
 void InitializeHTTP(webrtc_kvm*ctx){
 	ctx->http.auth.auth=new HttpAuthorization(ctx);
+	errno=0;
 	ctx->http.httpd=MHD_start_daemon(
 		MHD_USE_INTERNAL_POLLING_THREAD,
 		ctx->http.port,
 		nullptr,ctx,
 		HttpHandler::HandleConnection,ctx,
+		MHD_OPTION_EXTERNAL_LOGGER,handle_log,ctx,
 		MHD_OPTION_NOTIFY_COMPLETED,handle_complete,ctx,
 		MHD_OPTION_END
 	);
-	if(!ctx->http.httpd)throw RuntimeError("create http daemon failed");
+	if(!ctx->http.httpd)throw ErrnoException("create http daemon failed");
 	log_info("http server started at port {}",ctx->http.port);
 }
